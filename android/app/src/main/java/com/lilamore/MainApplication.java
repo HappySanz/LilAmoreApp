8e4bd359aa9b836618b930a19e2fa8025d9881ea
Nandhakumar <nandhakumaar.n@gmail.com>
2018-07-24 12:20:53 +0530

    updated

--- android/app/src/main/java/com/lilamore/MainApplication.java
+++ android/app/src/main/java/com/lilamore/MainApplication.java
@@ -34,10 +34,6 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
               new FBSDKPackage(mCallbackManager)
-<<<<<<< HEAD
-
-=======
->>>>>>> 966a6ae428982eae14c65df2acafb66f54d05f2e
       );
     }
 
