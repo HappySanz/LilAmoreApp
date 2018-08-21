b57d6d49363c08447b4a81bdfb2a47a8ceb074fc
Naren-HappySanz <devika.happysanz@gmail.com>
2018-08-21 17:12:38 +0530

    21-08

--- android/app/src/main/java/com/lilamore/MainApplication.java
+++ android/app/src/main/java/com/lilamore/MainApplication.java
@@ -36,9 +36,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage(),
             new ImagePickerPackage(),
-            new FBSDKPackage(),
 //            new RNGoogleSigninPackage(),
             new FBSDKPackage(mCallbackManager)
       );
